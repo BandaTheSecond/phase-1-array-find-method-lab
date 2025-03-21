c41352ea325f3b178b97a143079c7456a6e8327f
Kingbanda <bandadesignske@gmail.com>
2025-03-21 09:27:52 +0300

    Using find() method to get results

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  console.log(superbowlWin(record));
+
+function superbowlWin(records) {
+    let win = records.find(record => record.result === "W");
+    return win ? win.year : undefined;
+}
